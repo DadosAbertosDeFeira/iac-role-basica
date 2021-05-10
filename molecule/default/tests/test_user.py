0eec87be1573f9df7425738b3909fd5e11db99b3
Glenon Mateus <glenonmateus@gmail.com>
2021-05-10 12:44:38 -0300

    definida os diretórios do ansible no codeac

--- molecule/default/tests/test_user.py
+++ molecule/default/tests/test_user.py
@@ -1,12 +1,9 @@
 import os
- 
 import testinfra.utils.ansible_runner
  
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
  
- 
 def test_is_user_exists(host):
     user = host.user("gomex")
- 
     assert user.exists
